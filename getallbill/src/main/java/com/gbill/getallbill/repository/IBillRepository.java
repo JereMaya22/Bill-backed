274a19227e0ba31b5b7654a47a2417054965783e
JereMaya22 <jeremy.rivera409@gmail.com>
2025-08-29 18:14:59 -0600

    deleted getallbill old

--- getallbill/src/main/java/com/gbill/getallbill/repository/IBillRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gbill.getallbill.repository;
-
-import com.gbill.getallbill.model.Bill;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IBillRepository extends JpaRepository<Bill, Long> {
-}
--- getallbill/src/main/java/com/gbill/getallbill/service/IBillService.java
+++
@@ -1,9 +0,0 @@
-package com.gbill.getallbill.service;
-
-import com.gbill.getallbill.modeldto.BillAllDTO;
-
-import java.util.List;
-
-public interface IBillService {
-    List<BillAllDTO> getAllBills();
-}
