29efebd175b2135b82bdf60bb35d0b10a4b43c6d
JereMaya22 <jeremy.rivera409@gmail.com>
2025-08-29 18:14:12 -0600

    deleted getIdBill old

--- getIdBill/src/main/java/com/gbill/getIdBill/service/IBillService.java
+++
@@ -1,9 +0,0 @@
-package com.gbill.getIdBill.service;
-
-import com.gbill.getIdBill.modeldto.ShowBill;
-
-import java.util.Optional;
-
-public interface IBillService {
-    Optional<ShowBill> getById(String codeGenerator);
-}
